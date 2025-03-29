3020bd5c7ca9a608b7feffce466d4d799cc81a69
joan <joan@debook.168.1.18>
2025-03-29 16:27:23 +0100

    WIP

--- sys.c
+++ sys.c
@@ -2,6 +2,7 @@
  * sys.c - Syscalls implementation
  */
 #include "include/interrupt.h"
+#include "include/mm_address.h"
 #include <devices.h>
 
 #include <utils.h>
@@ -23,135 +24,101 @@ extern struct list_head freequeue, readyqueue;
 
 int pidGlobal = 1000;
 
-int ret_from_fork()
-{
+int ret_from_fork() { return 0; }
+
+int check_fd(int fd, int permissions) {
+  if (fd != 1)
+    return -9; /*EBADF*/
+  if (permissions != ESCRIPTURA)
+    return -13; /*EACCES*/
   return 0;
 }
 
-int check_fd(int fd, int permissions)
-{
-  if (fd!=1) return -9; /*EBADF*/
-  if (permissions!=ESCRIPTURA) return -13; /*EACCES*/
-  return 0;
-}
+int sys_ni_syscall() { return -38; /*ENOSYS*/ }
 
-int sys_ni_syscall()
-{
-	return -38; /*ENOSYS*/
-}
+int sys_getpid() { return current()->PID; }
 
-int sys_getpid()
-{
-	return current()->PID;
-}
-
-int sys_fork()
-{
-  //a) en el document:
-  if(list_empty(&freequeue)) return -ENOMEM; //Retorna un error de que no hi ha memoria 
-  
-  //S'agafa un PCB de la freequeue
-  struct list_head * e = list_first(&freequeue);
-  union task_union * fill = (union task_union *)list_head_to_task_struct(e);
+int sys_fork() {
+  if (list_empty(&freequeue))
+    return -ENOMEM;
+  struct list_head *e = list_first(&freequeue);
+  struct task_struct *hijo_task = list_head_to_task_struct(e);
   list_del(e);
-  
-  //b) en el document:
-  //Es fa una copia del task_union del pare en el fill
-  copy_data(current(), fill, sizeof(union task_union));
-  
-  //c) en el document:
-  //Obtenim un nou directori pel fill
-  allocate_DIR((struct task_struct *) fill);
-  
-  //d) en el document:
-  //Obtenim les pagines pel nostre fill
+  struct task_struct *padre_task = current();
+  copy_data(padre_task, hijo_task, sizeof(union task_union));
+
+  hijo_task->PID = pidGlobal++;
+  allocate_DIR(hijo_task);
+
+  // alloc de DATA al hijo
   int frames[NUM_PAG_DATA];
-  for(int i = 0; i < NUM_PAG_DATA; i++) {
-  	frames[i] = alloc_frame();
-  	
-  	if(frames[i] < 0) {
-  		//Si no hi ha mes espai, s'allibera el espai inutil que haviem reservat
-  		for(int j = 0; j < i; j++) {
-  			free_frame(frames[j]);
-  		}
-  		list_add_tail(&fill->task.list, &freequeue);
-  		return -ENOMEM;
-  	}
+  for (int i = 0; i < NUM_PAG_DATA; ++i) {
+    int id_frame = alloc_frame();
+    if (id_frame < 0) {
+      for (int j = 0; j < i; ++j) {
+        free_frame(frames[j]);
+      }
+      list_add_tail(e, &freequeue);
+      return -ENOMEM;
+    }
+    frames[i] = id_frame;
   }
-  
-  //e) en el document:
-  //Obtenim els punters de les TPs del pare i del fill
-  page_table_entry * fillTP = get_PT((struct task_struct *) fill);
-  page_table_entry * pareTP = get_PT(current());
-  
-  	//e) -> i)
-  	//Fer que pagines de kernel i codi apuntin a les del pare
-  for(int i = 0; i < NUM_PAG_KERNEL; i++) set_ss_pag(fillTP, i, get_frame(pareTP, i));
-  
-  for(int i = 0; i < NUM_PAG_CODE; i++) {
-  	set_ss_pag(fillTP, PAG_LOG_INIT_CODE + i, get_frame(pareTP, PAG_LOG_INIT_CODE + i));
+
+  page_table_entry *hijo_PT = get_PT(hijo_task);
+  page_table_entry *padre_PT = get_PT(padre_task);
+
+  // Enlaza el kernel:
+  for (int i = 0; i < NUM_PAG_KERNEL; ++i) {
+    unsigned int frame_kernel = get_frame(padre_PT, i);
+    set_ss_pag(hijo_PT, i, frame_kernel);
   }
-  
-  	//e) -> ii)
-  	//Vinculem les pagines logiques amb els frames abans agafats per data+stack
-  for(int i = 0; i < NUM_PAG_DATA; i++) set_ss_pag(fillTP, PAG_LOG_INIT_DATA + i, frames[i]);
-  
-  //RECORDATORI PER AQUEST APARTAT, S'HA IMPLEMENTAT D'UNA FORMA QUE ES MAPEJEN MOLTES PAGINES
-  //LOGIQUES AMB MOLTES FISIQUES, SI FUNCIONA, DESPRES PROVAR DE FER-HO AMB NOMES
-  //LA PAGINA LOGICA QUE VAGI DIRECTAMENT DESPRES DE NUM_PAG_KERNEL + NUM_PAG_CODE + NUM_PAG_DATA
-  //I ANAR FENT set_ss_pag(pareTP, LA PAGINA, get_frame(fillTP, i)) I DESPRES del_ss_pag(pareTP, LA PAGINA)
-  
-  //f) en el document:
-  //Creem una variable que ens fara accedir a les pagines lliures del pare
-  int offset = NUM_PAG_DATA + NUM_PAG_CODE;
-  
-  for(int i = PAG_LOG_INIT_DATA; i < (PAG_LOG_INIT_DATA + NUM_PAG_DATA); i++) {
-  	//Ara creem una pagina de copia que apunti al mateix frame que la DATA del fill
-  	set_ss_pag(pareTP, i + offset, get_frame(fillTP, i));
-  	//Ara copiem tot en aquesta pagina de copia vinculada al frame del DATA del fill perque aquest tingui la info
-  	copy_data((void *) (i << 12), (void *) ((i + offset) << 12), PAGE_SIZE);
-  	//Com ja tenim la info copiada, esborrem la pagina creada
-  	del_ss_pag(pareTP, i + offset);
+
+  // Enlaza el codigo:
+  for (int i = PAG_LOG_INIT_CODE; i < PAG_LOG_INIT_CODE + NUM_PAG_CODE; ++i) {
+    unsigned int frame_code = get_frame(padre_PT, i);
+    set_ss_pag(hijo_PT, i, frame_code);
   }
+
+  // Copia en mem libre data y pila
+  int init_free_mem = PAG_LOG_INIT_CODE + (NUM_PAG_CODE * PAGE_SIZE);
+
+  for(int i = 0; i < NUM_PAG_DATA*PAGE_SIZE; i+=PAGE_SIZE) {
+    set_ss_pag(padre_PT, init_free_mem + i, get_frame(hijo_PT, PAG_LOG_INIT_DATA + i));
+    copy_data(PAG_LOG_INIT_DATA + i, init_free_mem + i, PAGE_SIZE);
+    del_ss_pag(padre_PT, init_free_mem + i);
+  }
+
+  // Flush TLB
+  set_cr3(get_DIR(padre_task));
   
-  //Forçem flush en el TLB per borrar els mapejos temporals
-  set_cr3(get_DIR(current()));
-  
-  //g) en el document:
-  fill->task.PID = pidGlobal++;
-  
-  //i) en el document:
-  //posem el ebp fals de 0 i el ret_from_fork per posar a 0 el valor de retorn de la funcio
-  ((union task_union*)fill)->stack[KERNEL_STACK_SIZE-19] = (unsigned long) 0;
-  ((union task_union*)fill)->stack[KERNEL_STACK_SIZE-18] = (unsigned long) &ret_from_fork;
-  //fem que el kernel esp apunti al 0 del ebp per fer el truquillo
-  fill->task.k_esp = &((union task_union*)fill)->stack[KERNEL_STACK_SIZE-19];
+  ((union task_union *) hijo_task)->stack[KERNEL_STACK_SIZE-19] = (unsigned long) 0;
+  ((union task_union *) hijo_task)->stack[KERNEL_STACK_SIZE-18] = (unsigned long) &ret_from_fork;
+
+  hijo_task->k_esp = &((union task_union*)hijo_task)->stack[KERNEL_STACK_SIZE-19];
   
   //j) en el document:
-  list_add_tail(&(fill->task.list), &readyqueue);
+  list_add_tail(&(hijo_task->list), &readyqueue);
 
   //k) en el document:
-  return fill->task.PID;
+  return hijo_task->PID;
 }
 
-void sys_exit() 
-{  
+void sys_exit() {}
+
+int sys_write(int fd, char *buffer, int size) {
+  char bufferAux[size];
+  int checkFd = check_fd(fd, ESCRIPTURA);
+  if (checkFd)
+    return checkFd;
+  else if (buffer == NULL)
+    return EFAULT;
+  else if (size <= 0)
+    return EINVAL;
+  else {
+    copy_from_user(buffer, bufferAux, size);
+    int ret = sys_write_console(bufferAux, size);
+    return ret;
+  }
 }
 
-
-int sys_write(int fd, char * buffer, int size) {
-    char bufferAux[size];
-	int checkFd = check_fd(fd, ESCRIPTURA);
-	if(checkFd) return checkFd;
-	else if(buffer == NULL) return EFAULT;
-	else if (size <= 0) return EINVAL;
-	else {
-		copy_from_user(buffer, bufferAux, size);
-		int ret = sys_write_console(bufferAux, size);
-		return ret;
-	}
-}
-
-int sys_gettime() {
-  return zeos_tick;
-}
+int sys_gettime() { return zeos_tick; }
